24a8584763e1059c02687adb235d26c69d299668
renny5120 <renny5120@gmail.com>
2022-06-30 11:38:33 -0700

    add operators in javascript.js

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Calculator
+This project will create a functional calculator webpage. Users will be able to input numbers and operations to get values. 
---
+++ javascript.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(op, a, b) {
+    switch(op){
+        case '+':
+            return add(a,b);
+
+        case '-':
+            return subtract(a,b);
+
+        case '*':
+            return multiply(a,b);
+
+        case '/':
+            return divide(a,b);
+
+    }
+}
+
+console.log(operate('/', 8, 2));
