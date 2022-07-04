32f035ad9d940cdfbed0a5bed495f531da97e158
renny5120 <renny5120@gmail.com>
2022-07-04 11:53:12 -0700

    add functional operations

--- javascript.js
+++ javascript.js
@@ -15,20 +15,103 @@ function divide(a, b) {
 }
 
 function operate(op, a, b) {
-    switch(op){
+    switch (op) {
         case '+':
-            return add(a,b);
+            return add(a, b);
 
         case '-':
-            return subtract(a,b);
+            return subtract(a, b);
 
-        case '*':
-            return multiply(a,b);
+        case 'x':
+            return multiply(a, b);
 
         case '/':
-            return divide(a,b);
+            return divide(a, b);
 
     }
 }
 
-console.log(operate('/', 8, 2));
+
+// text for display div
+const display = document.querySelector('.display');
+let displayNum = '';
+let clearDisplay = false;
+display.textContent = displayNum;
+
+// listener for numbers
+const numbers = document.querySelectorAll('.numbers');
+numbers.forEach((div) => {
+    div.addEventListener('click', function (e) {
+        // ignores the "numbers" div
+        if (e.target.innerText.length > 1) {
+            return;
+        }
+        // clears display if operator was clicked before
+        if (clearDisplay == true) {
+            displayNum = '';
+            clearDisplay = false;
+        }
+        // adds number to display
+        displayNum += e.target.innerText;
+        display.textContent = displayNum;
+    });
+});
+
+// storing operator and number
+let savedOp = '';
+let savedNum = '';
+
+// listener for operators
+const operators = document.querySelectorAll('.operators');
+operators.forEach((div) => {
+    div.addEventListener('click', function (e) {
+        let operation = e.target.innerText;
+        clearDisplay = true;
+
+        if(savedOp != '' && savedNum != ''){
+            displayNum = operate(savedOp, Number(savedNum), Number(displayNum));
+            console.log(displayNum);
+            display.textContent = displayNum;
+            savedOp = '';
+        }
+
+        // clear
+        if (operation == 'clear') {
+            savedOp = '';
+            savedNum = '';
+            displayNum = '';
+            display.textContent = displayNum;
+            return;
+        }
+
+        // add
+        if (operation == '+') {
+            savedOp = '+';
+            savedNum = displayNum;
+            return;
+        }
+
+        // subtract
+        if (operation == '-') {
+            savedOp = '-';
+            savedNum = displayNum;
+            return;
+        }
+
+        // multiply
+        if (operation == 'x') {
+            savedOp = 'x';
+            savedNum = displayNum;
+            return;
+        }
+
+        // divide
+        if (operation == '/') {
+            savedOp = '/';
+            savedNum = displayNum;
+            return;
+        }
+    });
+});
+
+
